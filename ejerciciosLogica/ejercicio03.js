43f0c43bfb14b1f4f7068b05a98c0867abfd79ea
Luisa Ramirez <104102652+luimos-ramirez@users.noreply.github.com>
2023-07-02 20:53:45 -0500

    Se valida que en el array solo se ingresen unicamente datos de tipo string

--- ejerciciosLogica/ejercicio03.js
+++ ejerciciosLogica/ejercicio03.js
@@ -1,6 +1,9 @@
 const nombre = ['Juan', 'Maria', 'Pedro', 'Juan','Juan', 'Maria', 'Maria']
 
 function countNameRepetitions(nombres) {
+    if(nombres.filter(value => typeof value !== 'string').length > 0){
+        return "Los valores ingresados en el array deben ser de tipo string.";
+    }
     let result={};
     for (var i = 0; i < nombres.length; i++)
     { 
